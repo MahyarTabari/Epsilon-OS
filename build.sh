2d791b49d53aa2153a28643ee35975e5f72bbdf6
mahyar <mahyartabarii@gmail.com>
2023-08-06 18:19:01 +0330

    build.sh file is improved

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
+# needed for the cross compiler used in Makefile
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
+
+# if ./build or ./bin directories don't exist, create them
+# these are needed for build process
+if ![ -d ./build ]; then
+	mkdir bin
+
+if ![ -d ./bin ]; then
+	mkdir bin
+
 
 make all
